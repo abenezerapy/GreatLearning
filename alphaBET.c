26db94a876356717d02fd7683800ddaa2239660e
abenezerapy <106971846+abenezerapy@users.noreply.github.com>
2022-08-25 12:01:11 +0300

    alphaBET.c added

---
+++ alphaBET.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main()
+{
+    int ch;
+    int sch;
+
+    for (ch = 65; ch < 91; ch++)
+    {
+        putchar(ch);
+    }
+    for (sch = 97; sch < 123; sch++)
+    {
+        putchar(sch);
+    }
+    
+    
+}
